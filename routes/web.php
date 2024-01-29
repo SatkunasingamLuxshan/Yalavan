8b4b901b95c0bb2ebeb32c41812951436a3fa7ae
SatkunasingamLuxshan <luxshanlux2@gmail.com>
2024-01-29 10:15:03 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 // Route::get('/404', function () {
 //     return view('frontend.404');
